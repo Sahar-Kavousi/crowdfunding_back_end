e8768df24ae796b08cbe22510cd47019bc2492ad
Sahar Kavousi <s.ka1366@gmail.com>
2024-01-13 10:21:01 +0800

    Done Handling Model Relationships

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -8,3 +9,15 @@ class Project(models.Model):
     is_open = models.BooleanField()
     date_created = models.DateTimeField()
     owner = models.CharField(max_length=200)
+
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Pledge
+
+
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pledge
+        fields = '__all__'
+
 
 class ProjectSerializer(serializers.ModelSerializer):
-    class Meta: 
-        model = Project        
-        fields ='__all__'
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class ProjectDetailSerializer(ProjectSerializer): 
+    pledges = PledgeSerializer(many=True, read_only=True)
