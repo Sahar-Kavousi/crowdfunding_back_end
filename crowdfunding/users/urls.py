6b696cbeacddc1a1923f9ea985f30d00e06e3dba
Sam Gharcheh <sam.gharacheh@outlook.com>
2024-03-03 15:37:24 +0800

    fixed the get method of the get user info and URLs

--- crowdfunding/users/urls.py
+++ crowdfunding/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/', views.CustomUserList.as_view()),
-    path('users/current/', views.CustomUserDetail.as_view()),
-    path('users/<int:pk>/', views.CustomUserDetail.as_view()),
+    path('user/', views.CustomUserDetail.as_view(), name='user-detail'),
+    path('user/<int:pk>/', views.CustomUserDetail.as_view(), name='specific-user-detail'),
     ]
 
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -5,33 +5,32 @@ from rest_framework import status
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
+
 class CustomUserList(APIView):
     def get(self, request):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
-               serializer.save()
-               return Response(serializer.data)
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class CustomUserDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return CustomUser.objects.get(pk=pk)
-        except CustomUser.DoesNotExist:
-              raise Http404
-        
-    def get(self, request, pk):
+    def get_object(self, pk=None):
+        if pk is not None:
+            try:
+                return CustomUser.objects.get(pk=pk)
+            except CustomUser.DoesNotExist:
+                raise Http404
+        else:
+            return self.request.user
+
+    def get(self, request, pk=None):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
-
-    def get(self, request):
-        user = self.request.user
-        serializer = CustomUserSerializer(user)
-        return Response(serializer.data)
-    
